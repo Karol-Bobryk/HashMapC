a098465caa4b87366cffdf88fbb22d9b7d025f1d
Karol-Bobryk <karolekb2002@gmail.com>
2024-07-08 09:14:00 +0200

    fixed demo in main

--- main.c
+++ main.c
@@ -1,15 +1,15 @@
 #include "hashMap.h"
 #include <stdio.h>
 int main() {
-  HashMap *hm = initHM();
-  appendHM(hm, "key1", "value1");
-  appendHM(hm, "key5", "value5");
-  appendHM(hm, "key2", "value2");
-  appendHM(hm, "key3", "value3");
-  appendHM(hm, "key3", "value69");
-  appendHM(hm, "key4", "value4");
-  printf("\n%s\n", readHM(hm, "key4"));
-  printHM(hm);
-  freeHM(hm);
+  HashMap *hm = HMInit();
+  HMAppend(hm, "key1", "value1");
+  HMAppend(hm, "key5", "value5");
+  HMAppend(hm, "key2", "value2");
+  HMAppend(hm, "key3", "value3");
+  HMAppend(hm, "key3", "value69");
+  HMAppend(hm, "key4", "value4");
+  printf("\n%s\n", HMRead(hm, "key4"));
+  HMPrint(hm);
+  HMFree(hm);
   return 0;
 }
